bf9b7ddc33a7239445443520dd76b58255b8eeda
pricenm <pricenm@LCBLAB3120J-24.uccob.uc.edu>
2023-04-20 12:13:36 -0400

    commit

---
+++ pricenm_Assignment02/main_assignment02.py
@@ -0,0 +1,33 @@
+# Name: Nicole Price
+# email: pricenm@mail.uc.edu
+# Assignment Title: Assignment 02
+# Course: IS 4010
+# Semester/Year: Spring 2023
+# Brief Description: This project attempts to meet the requirements laid out in the document
+# Citations: https://stackoverflow.com/questions/16451978/extracting-single-letters-out-of-a-string-in-python &
+# Anything else that's relevant:
+
+# 1. Store fizz and print fizz -1
+fizz = 99
+print(fizz - 1)
+
+# 2. Store gamma and print gamma content plus fizz contents
+gamma = ("University of Southern Ohio")
+print(gamma, fizz)
+
+# 3. Store delta, add fizz and delta, store under epsilon and print epsilon
+delta = 1.5
+epsilon = fizz + delta
+print(epsilon)
+
+# 4. compose an expression that extracts the 'v' in the variable gamma, store that result in eta
+# and print the contents of eta bracketed by ">" and "<"
+eta = gamma[3]
+print(">",eta,"<")
+
+# 5. store 1234.5 in nu, print nu contents to console,  write an expression that adds contents of delta
+# and fizz stores the results in zeta and print the value of zeta to the console
+nu = 1234.5
+print(nu)
+zeta = delta + fizz
+print(zeta)
